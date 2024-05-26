dd31d3274b59159d357c79061294e9f83842449a
Matheus Quost <matheusquost@gmail.com>
2024-05-26 01:03:46 -0300

    feat: Add authentication and authorization to candidate login

--- src/main/java/io/github/mqdev/front_gestao_vagas/modules/candidate/controller/CandidateController.java
+++ src/main/java/io/github/mqdev/front_gestao_vagas/modules/candidate/controller/CandidateController.java
@@ -1,6 +1,8 @@
 package io.github.mqdev.front_gestao_vagas.modules.candidate.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import io.github.mqdev.front_gestao_vagas.modules.candidate.service.CandidateService;
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+
 
 @Controller
 @RequestMapping("/candidate")
@@ -22,17 +31,29 @@ public class CandidateController {
     }
 
     @PostMapping("/signin")
-    public String signin(RedirectAttributes redirectAttributes, String username, String password) {
+    public String signin(RedirectAttributes redirectAttributes, HttpSession session, String username, String password) {
         try {
-            candidateService.login(username, password);
-            return "candidate/profile";
+            var authInfo = candidateService.login(username, password);
+            var roles = authInfo.getRoles().stream().map(role -> new SimpleGrantedAuthority("ROLE_" + role.toString().toUpperCase())).toList();
+
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(username, password, roles);
+            auth.setDetails(authInfo);
+
+            SecurityContextHolder.getContext().setAuthentication(auth);
+            SecurityContext context = SecurityContextHolder.getContext();
+            session.setAttribute("SPRING_SECURITY_CONTEXT", context);
+            session.setAttribute("authInfo", authInfo);
+
+            return "redirect:/candidate/profile";
         } catch (Exception e) {
             redirectAttributes.addFlashAttribute("error", "Usuário ou senha inválidos");
             return "redirect:/candidate/login";
         }
     }
 
+
     @GetMapping("/profile")
+    @PreAuthorize("hasRole('CANDIDATE')")
     public String Profile() {
         return "candidate/profile";
     }
