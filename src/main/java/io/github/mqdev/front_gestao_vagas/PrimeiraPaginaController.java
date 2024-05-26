d91d3a4a6b75e282e60bf564e518c8ce3a04ee2a
Matheus Quost <matheusquost@gmail.com>
2024-05-25 23:04:55 -0300

    chore: Update login form action URL to use Thymeleaf syntax

--- src/main/java/io/github/mqdev/front_gestao_vagas/PrimeiraPaginaController.java
+++ src/main/java/io/github/mqdev/front_gestao_vagas/PrimeiraPaginaController.java
@@ -1,7 +1,9 @@
 package io.github.mqdev.front_gestao_vagas;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,12 +11,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PrimeiraPaginaController {
     
     @GetMapping("/home")
-    public String homeHtml() {
+    public String homeHtml(Model model) {
+        model.addAttribute("mensagemDaController", "Primeira mensagem da controller");
         return "primeiraPagina";
     }
 
     @GetMapping("/login")
     public String loginHtml() {
+        return "candidate/login";
+    }
+
+    @PostMapping("/login")
+    public String loginCandidate(String email, String password) {
+        System.out.println("Email: " + email);
+        System.out.println("Password: " + password);
         return "candidate/login";
     }
 }
