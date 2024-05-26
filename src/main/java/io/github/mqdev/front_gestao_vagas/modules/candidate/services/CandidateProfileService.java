af6e2c77dede2e971e73a15ca4994198a10562dd
Matheus Quost <matheusquost@gmail.com>
2024-05-26 01:37:23 -0300

    refactor: Update CandidateProfileService to return CandidateProfileDTO

--- src/main/java/io/github/mqdev/front_gestao_vagas/modules/candidate/services/CandidateProfileService.java
+++ src/main/java/io/github/mqdev/front_gestao_vagas/modules/candidate/services/CandidateProfileService.java
@@ -6,12 +6,14 @@ import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.mqdev.front_gestao_vagas.modules.candidate.dto.CandidateProfileDTO;
+
 import java.util.Map;
 
 @Service
 public class CandidateProfileService {
 
-    public String getCandidateProfile(String token) {
+    public CandidateProfileDTO getCandidateProfile(String token) {
         RestTemplate restTemplate = new RestTemplate();
 
         String url = "http://34.82.61.35:8080/candidate/";
@@ -21,7 +23,7 @@ public class CandidateProfileService {
 
         HttpEntity<Map<String, String>> request = new HttpEntity<>(headers);
 
-        return restTemplate.exchange(url, HttpMethod.GET, request, String.class).getBody();
+        return restTemplate.exchange(url, HttpMethod.GET, request, CandidateProfileDTO.class).getBody();
     }
     
 }
