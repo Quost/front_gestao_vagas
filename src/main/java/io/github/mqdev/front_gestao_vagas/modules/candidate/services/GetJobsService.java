f159576b10efa84051ddca98a8024778ae7bbc9e
Matheus Quost <matheusquost@gmail.com>
2024-05-26 02:32:37 -0300

    feat: Add GetJobsService for retrieving jobs in candidate module

---
+++ src/main/java/io/github/mqdev/front_gestao_vagas/modules/candidate/services/GetJobsService.java
@@ -0,0 +1,45 @@
+package io.github.mqdev.front_gestao_vagas.modules.candidate.services;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpClientErrorException.Unauthorized;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import io.github.mqdev.front_gestao_vagas.modules.candidate.dto.JobDTO;
+
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+import java.util.List;
+
+@Service
+public class GetJobsService {
+
+    public List<JobDTO> getJobs(String token, String filter) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        String url = "http://34.82.61.35:8080/candidate/job";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(token);
+
+        HttpEntity<Map<String, String>> request = new HttpEntity<>(headers);
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url)
+                .queryParam("filter", filter);
+
+        ParameterizedTypeReference<List<JobDTO>> responseType = new ParameterizedTypeReference<List<JobDTO>>() {
+        };
+
+        try {
+            return restTemplate.exchange(builder.toUriString(), HttpMethod.GET, request, responseType).getBody();
+        } catch (Unauthorized e) {
+            throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED, "Token inválido");
+        }
+    }
+}
